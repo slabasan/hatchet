6eb2290a76fe2c5150aecbf4086a8c5f6dd9ac1a
Stephanie Brink <brink2@llnl.gov>
2019-08-14 09:05:48 -0700

    WIP graph union
    - graph union implementation
    - add graph test for computing the union of two structurally identical and
      non-identical graphs

--- hatchet/graph.py
+++ hatchet/graph.py
@@ -15,6 +15,7 @@ import sys
 from .external.printtree import trees_as_text
 from .util.dot import trees_to_dot
 from .node import Node
+from .frame import Frame
 
 
 class Graph:
@@ -34,14 +35,14 @@ class Graph:
                 # do a breadth-first traversal of the tree
                 for node in root.traverse_bf():
                     if not node.parents:
-                        clone = Node(node.nid, tuple(node.callpath), None)
+                        clone = Node(Frame(node.frame.attrs.copy()), None)
                         list_roots.append(clone)
                     else:
                         # make a list of new parents to make connections with
                         new_parents = []
                         for parent in node.parents:
                             new_parents.append(node_clone[parent])
-                        clone = Node(node.nid, tuple(node.callpath), None)
+                        clone = Node(Frame(node.frame.attrs.copy()), None)
 
                         # connect child to parents and vice-versa
                         for parent in new_parents:
@@ -51,6 +52,133 @@ class Graph:
                     node_clone[node] = clone
 
         return (Graph(list_roots), node_clone)
+
+    def union(self, other):
+        """Unify two graphs by comparing frame at each node and return a single
+        unified graph and a mapping from self nodes to other nodes.
+        """
+        def node_union(union_nodes, subgraph, other_subgraph, vs=None, vo=None):
+            if vs is None:
+                vs = set()
+            if vo is None:
+                vo = set()
+
+            vs.add(id(subgraph))
+            vo.add(id(other_subgraph))
+
+            ssorted = sorted(subgraph.children, key=lambda x: x.frame)
+            osorted = sorted(other_subgraph.children, key=lambda x: x.frame)
+
+            for ch, other_ch in zip(ssorted, osorted):
+                # if node is not in union graph, add it
+                if ch.frame not in union_nodes:
+                    union_nodes[ch.frame] = ch
+                if other_ch.frame not in union_nodes:
+                    union_nodes[other_ch.frame] = other_ch
+
+                visited_s = id(ch) in vs
+                visited_o = id(other_ch) in vo
+
+                # skip visited nodes
+                if visited_s or visited_o:
+                   continue
+
+                # if subgraph node already exists in union graph, then append its
+                # children and parents to the node in union graph
+                if ch.frame in union_nodes:
+                    for p1 in ch.parents:
+                        # add parent if it is not in master graph
+                        has_parent = False
+                        for i in union_nodes[ch.frame].parents:
+                            if p1.frame == i.frame:
+                                has_parent = True
+                                break
+                        if not has_parent:
+                            union_nodes[ch.frame].add_parent(p1)
+                    for c1 in ch.children:
+                        # add child if it is not in master graph
+                        has_child = False
+                        for i in union_nodes[ch.frame].children:
+                            if c1.frame == i.frame:
+                                has_child = True
+                                break
+                        if not has_child:
+                            union_nodes[ch.frame].add_child(c1)
+
+                # if other_subgraph node already exists in union graph, then
+                # append its children and parents to the node in union graph
+                if other_ch.frame in union_nodes:
+                    for p2 in other_ch.parents:
+                        # add parent if it is not in master graph
+                        has_parent = False
+                        for i in union_nodes[other_ch.frame].parents:
+                            if p2.frame == i.frame:
+                                has_parent = True
+                                break
+                        if not has_parent:
+                            union_nodes[other_ch.frame].add_parent(p2)
+                    for c2 in other_ch.children:
+                        # add child if it is not in master graph
+                        has_child = False
+                        for i in union_nodes[other_ch.frame].children:
+                            if c2.frame == i.frame:
+                                has_child = True
+                                break
+                        if not has_child:
+                            union_nodes[other_ch.frame].add_child(c2)
+
+                # update union nodes with parents and children
+                ch.parents = union_nodes[ch.frame].parents
+                ch.children = union_nodes[ch.frame].children
+
+                other_ch.parents = union_nodes[other_ch.frame].parents
+                other_ch.children = union_nodes[other_ch.frame].children
+
+                node_union(union_nodes, ch, other_ch, vs, vo)
+
+        union_roots = []
+        union_nodes = {}
+
+        # if graphs are structurally equivalent, return a copy of self.graph
+        if self == other:
+            graph_copy, _ = self.copy()
+            return graph_copy
+        # if graphs are not structurally equivalent, return a new graph
+        elif self != other:
+            vs = set()
+            vo = set()
+
+            # sort roots by its frame
+            ssorted = sorted(self.roots, key=lambda x: x.frame)
+            osorted = sorted(other.roots, key=lambda x: x.frame)
+
+            for self_root, other_root in zip(ssorted, osorted):
+                # if list of roots is empty, append root
+                if not union_roots:
+                    union_roots.append(self_root)
+                # otherwise, loop over list of roots to see if self_root or
+                # other_root are already in the list, appending them if they
+                # are not
+                else:
+                    has_node = False
+                    for i in union_roots:
+                        if self_root.frame == i.frame:
+                            has_node = True
+                            break
+                    if not has_node:
+                        union_roots.append(self_root)
+
+                    has_node = False
+                    for i in union_roots:
+                        if other_root.frame == i.frame:
+                            has_node = True
+                            break
+                    if not has_node:
+                        union_roots.append(other_root)
+
+                node_union(union_nodes, self_root, other_root, vs, vo)
+
+            return Graph(union_roots)
 
     def to_string(
         self,
--- hatchet/tests/graph_ops.py
+++ hatchet/tests/graph_ops.py
@@ -41,3 +41,77 @@ def test_dag_not_equal(mock_dag_literal1, mock_dag_literal2):
     other.from_literal(mock_dag_literal2)
 
     assert gf.graph != other.graph
+
+
+def test_union_dag_same_structure(mock_dag_literal1):
+    # make graphs g1 and g2 that you know are equal
+    gf = GraphFrame()
+    gf.from_literal(mock_dag_literal1)
+
+    other = GraphFrame()
+    other.from_literal(mock_dag_literal1)
+
+    g1 = gf.graph
+    g2 = other.graph
+
+    assert g1 == g2
+
+    g3 = g1.union(g2)
+    assert g3 is not g1
+    assert g3 is not g2
+    assert g3 == g1
+    assert g3 == g2
+
+
+def test_union_dag_different_structure(mock_dag_literal1, mock_dag_literal2):
+    # make graphs g1, g2, and g3, where you know g3 is the union of g1 and g2
+    gf = GraphFrame()
+    gf.from_literal(mock_dag_literal1)
+
+    other = GraphFrame()
+    other.from_literal(mock_dag_literal2)
+
+    known_union = GraphFrame()
+    known_union.from_literal(
+        [
+            {
+                "name": "A",
+                "metrics": {"time (inc)": 130.0, "time": 0.0},
+                "children": [
+                    {
+                        "name": "B",
+                        "metrics": {"time (inc)": 20.0, "time": 5.0},
+                        "children": [
+                            {
+                                "name": "C",
+                                "metrics": {"time (inc)": 5.0, "time": 5.0},
+                                "children": [
+                                    {
+                                        "name": "D",
+                                        "metrics": {"time (inc)": 8.0, "time": 1.0},
+                                    }
+                                ],
+                            }
+                        ],
+                    },
+                    {
+                        "name": "E",
+                        "metrics": {"time (inc)": 55.0, "time": 10.0},
+                        "children": [
+                            {"name": "F", "metrics": {"time (inc)": 1.0, "time": 9.0}},
+                            {"name": "H", "metrics": {"time (inc)": 1.0, "time": 9.0}}
+                        ],
+                    },
+                ],
+            }
+        ]
+    )
+
+    g1 = gf.graph
+    g2 = other.graph
+    g3 = known_union.graph
+
+    assert g1 != g2
+
+    g4 = g1.union(g2)
+    assert g4 == g3
